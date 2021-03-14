d65a683b289287312cd4702247a13c104026ebab
Quantumfram <quantumfram@gmail.com>
2021-03-14 20:31:32 +0800

    Manage Employee

--- src/main/java/com/wilsonovo/springboot/controller/EmployeeController.java
+++ src/main/java/com/wilsonovo/springboot/controller/EmployeeController.java
@@ -1,10 +1,15 @@
 package com.wilsonovo.springboot.controller;
 
+import com.wilsonovo.springboot.dao.DepartmentDao;
 import com.wilsonovo.springboot.dao.EmployeeDao;
+import com.wilsonovo.springboot.pojo.Department;
 import com.wilsonovo.springboot.pojo.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Collection;
@@ -15,6 +20,9 @@ public class EmployeeController {
     @Autowired
     EmployeeDao employeeDao;
 
+    @Autowired
+    DepartmentDao departmentDao;
+
     @RequestMapping("/employees")
     public String list(Model model){
         Collection<Employee> employees = employeeDao.getAll();
@@ -22,4 +30,31 @@ public class EmployeeController {
         return "employees/list";
     }
 
+    @GetMapping("/addEmployee")
+    public String toAddPage(Model model){
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("departments",departments);
+        return "employees/add";
+    }
+
+    @PostMapping("/addEmployee")
+    public String addEmployee(Employee employee){
+        employeeDao.add(employee);
+        return "redirect:/employees";
+    }
+
+    @GetMapping("/editEmployee/{id}")
+    public String toEditPage(@PathVariable("id")Integer id, Model model){
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("departments",departments);
+        Employee employee = employeeDao.getEmployeeById(id);
+        model.addAttribute("employee",employee);
+        return "employees/edit";
+    }
+
+    @PostMapping("/editEmployee")
+    public String editEmployee(Employee employee){
+        employeeDao.add(employee);
+        return "redirect:/employees";
+    }
 }
